587c8ad7be612f7a6adb1f4ed143462a799223b3
prandog <115602023+prandog@users.noreply.github.com>
2025-01-25 16:27:34 -0800

    login page

---
+++ frontend/src/setup.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE travel_db;
+
+USE travel_db;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
+);
